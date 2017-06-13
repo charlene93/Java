a49c1af1acabad66de68b09592825db2b3425c8d
charlene93 <charlie.chen93@foxmail.com>
2017-06-13 11:00:40 +0800

    Initial commit

---
+++ net/src/com/netstudy/www/SocketServer.java
@@ -0,0 +1,10 @@
+package com.netstudy.www;
+
+/**
+ * Created by cheny on 2017/5/30 0030.
+ */
+public class SocketServer {
+    public static void main(String[] args){
+        System.out.print("server");
+    }
+}
